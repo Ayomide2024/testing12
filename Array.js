e4ec4091bb815530c94660cd5f35f051c6cd4406
J-boi <134131113+Ayomide2024@users.noreply.github.com>
2023-12-02 15:38:16 -0700

    Add files via upload

---
+++ Array.js
@@ -0,0 +1,53 @@
+// Array data type
+
+
+
+
+const btn =document.getElementById('mybtn1');
+btn.addEventListener('click', clickme);
+function clickme(){
+    document.getElementById('body').style.backgroundColor='white'
+}
+const btns =document.getElementById('mybtn2');
+btns.addEventListener('click', click);
+function click(){
+    document.getElementById('body').style.backgroundColor='black'
+}
+const btnst =document.getElementById('mybtn3');
+btnst.addEventListener('click', clickus);
+function clickus(){
+    document.getElementById('body').style.backgroundColor='grey'
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let number = [1, 2, 3, 4, 5, 6, 7]
+number[7]=8
+number[8]
+console.log(number);
